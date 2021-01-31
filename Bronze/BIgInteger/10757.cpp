41f9419211ef863865ead0b5629ffc69ae37532d
Jun Hyuk Lim <59442344+limjunhyuk97@users.noreply.github.com>
2021-01-31 23:40:08 +0900

    Update 10757.cpp

--- Bronze/BIgInteger/10757.cpp
+++ Bronze/BIgInteger/10757.cpp
@@ -1,117 +1,60 @@
-// int형은 21억까지 표현가능하다. == 사실상 9자리이고, 최대 10자리까지 표현가능
-// long은 922경까지 표현가능하다. == 사실상 18자리이고, 최대 19자리까지 표현가능
-
 #include <iostream>
 #include <string>
 using namespace std;
 
-class BigInteger {
-private:
-	string num;
-	int* splitnum;
-	int splitnumlen;
-public:
-	BigInteger(string num) : num(num) {
-		MakeSplitnum();
-	}
-
-	void MakeSplitnum() {
-		int rem = num.size() % 5;
-		if (rem == 0) splitnumlen = (num.size() / 5);
-		else splitnumlen = (num.size() / 5) + 1;
-
-		splitnum = new int[splitnumlen] {};
-		for (int i = 0; i < splitnumlen; ++i) {
-			if (rem == 0) {
-				splitnum[splitnumlen - i - 1] = stoi(num.substr(5 * i + rem, 5));
-			}
-			else {
-				if (i == 0) {
-					splitnum[splitnumlen - i - 1] = stoi(num.substr(0, rem));
-				}
-				else {
-					splitnum[splitnumlen - i - 1] = stoi(num.substr(5 * (i - 1) + rem, 5));
-				}
-			}
-		}
-	}
-
-	int OutSplitnumlen() const{
-		return splitnumlen;
-	}
-
-	int* OutSplitnum() const {
-		return splitnum;
-	}
-
-	~BigInteger() {
-		delete[]splitnum;
-	}
-};
-
-class BigIntegerOperator {
-private:
-	BigInteger &big1;
-	BigInteger &big2;
-public:
-	BigIntegerOperator(BigInteger &big1, BigInteger &big2) : big1(big1), big2(big2) {}
-
-	string AddBigInteger() {
-
-		int maxsize = (big1.OutSplitnumlen() >= big2.OutSplitnumlen() ? big1.OutSplitnumlen() : big2.OutSplitnumlen());
-		int* ansnum = new int[maxsize + 1]{};
-		string ansstr;
-
-		for (int i = 0; i < maxsize; ++i) {
-			if (big1.OutSplitnumlen() <= i || big2.OutSplitnumlen()<=i) {
-				if (big1.OutSplitnumlen() <= i && big2.OutSplitnumlen() <= i) continue;
-				else if (big1.OutSplitnumlen() <= i) ansnum[maxsize - i - 1] += *(big2.OutSplitnum() + i);
-				else if (big2.OutSplitnumlen() <= i) ansnum[maxsize - i - 1] += *(big1.OutSplitnum() + i);
-			}
-			else {
-				if (*(big1.OutSplitnum() + i) + *(big2.OutSplitnum() + i) + ansnum[maxsize - 1 - i] > 100000) {
-					ansnum[maxsize - 1 - i] += *(big1.OutSplitnum() + i) + *(big2.OutSplitnum() + i) - 100000;
-					ansnum[maxsize - 1 - i - 1] += 1;
-				}
-				else {
-					ansnum[maxsize - 1 - i] += *(big1.OutSplitnum() + i) + *(big2.OutSplitnum() + i);
-				}
-			}
-		}
-		
-		for (int i = 0; i < maxsize; ++i) {
-			string tmp = to_string(ansnum[i]);
-			for (int i = 0; i < 5; ++i) {
-				if (i < 5 - tmp.size()) ansstr.push_back('0');
-				else {
-					ansstr += tmp; break;
-				}
-			}
-		}
-		
-		int cnt = 0;
-		while (1) {
-			if (ansstr[cnt] == '0')
-				++cnt;
-			else break;
-		}
-
-		return ansstr.substr(cnt);
-	}
-
-};
+string str1, str2, ans;
 
 int main(void) {
 
-	string str1, str2;
 	cin >> str1 >> str2;
+	int len;
+	
+	// str2 > str1
+	if (str1.size() < str2.size()) {
+		int tmp = str1.size();
+		len = str2.size();
+		int end = len;
+		str1.resize(len);
+		while (tmp != 0) {
+			str1[--end] = str1[--tmp];
+		}
+		for (int i = 0; i < end; ++i)
+			str1[i] = '0';
 
-	BigInteger big1(str1);
-	BigInteger big2(str2);
-	BigIntegerOperator OP(big1, big2);
-	cout<<OP.AddBigInteger();
+	}
+
+	// str1 > str2
+	else if (str1.size() > str2.size()) {
+		int tmp = str2.size();
+		len = str1.size();
+		int end = len;
+		str2.resize(len);
+		while (tmp != 0) {
+			str2[--end] = str2[--tmp];
+		}
+		for (int i = 0; i < end; ++i)
+			str2[i] = '0';
+	}
+	else {
+		len = str1.size();
+	}
+	ans.resize(len + 1, '0');
+
+	for (int i = len; i >= 1; --i) {
+		int tmp = (ans[i] - 48) + (str1[i - 1] - 48) + (str2[i - 1] - 48);
+		if (tmp >= 10) {
+			ans[i] = tmp + 38;
+			ans[i - 1] = 49;
+		}
+		else {
+			ans[i] = tmp + 48;
+		}
+	}
+
+	if (ans[0] == '0')
+		cout << ans.substr(1);
+	else
+		cout << ans;
 
 	return 0;
 }
-
-// 이걸로 안풀림. 수정해야할 듯..
