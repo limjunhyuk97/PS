0860293ff8966ebbf7941aec2d1f56fccc17838f
Jun Hyuk Lim <59442344+limjunhyuk97@users.noreply.github.com>
2021-02-28 13:01:58 +0900

    Update 1149.cpp

--- SILVER/1149_DP/1149.cpp
+++ SILVER/1149_DP/1149.cpp
@@ -1,43 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int RGB[1001][3];
+
 int main(void) {
 
 	int N; cin >> N;
-	int* arr = new int[N];
-	int* arrC = new int[N];
-
-	for (int i = 0; i < N; ++i) {
-		int min = 1001;
-		for (int j = 0; j < 3; ++j) {
-			int tmp;
-			cin >> tmp;
-			if (i == 0) {
-				if (min > tmp) {
-					min = tmp;
-					arr[i] = min;
-					arrC[i] = j;
-				}
-			}
-			else {
-				for (int k = 0; k < i; ++k) {
-					if ((tmp < min) && (arrC[k] != j)) {
-						min = tmp;
-						arr[i] = min;
-						arrC[i] = j;
-					}
-				}
-			}
-		}
+	for (int i = 1; i <= N; ++i) {
+		int R, G, B; cin >> R >> G >> B;
+		RGB[i][0] = (RGB[i - 1][1] >= RGB[i - 1][2] ? RGB[i - 1][2] : RGB[i - 1][1]) + R;
+		RGB[i][1] = (RGB[i - 1][0] >= RGB[i - 1][2] ? RGB[i - 1][2] : RGB[i - 1][0]) + G;
+		RGB[i][2] = (RGB[i - 1][1] >= RGB[i - 1][0] ? RGB[i - 1][0] : RGB[i - 1][1]) + B;
 	}
 
-	int sum = 0;
-	for (int i = 0; i < N; ++i) {
-		sum += arr[i];
-	}
-	cout << sum;
-
-	delete[]arr; delete[]arrC;
+	int min;
+	min = RGB[N][0] >= RGB[N][1] ? RGB[N][1] : RGB[N][0];
+	min = min >= RGB[N][2] ? RGB[N][2] : min;
+	cout << min;
 
 	return 0;
 }
