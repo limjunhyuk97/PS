cc2a722cce2e90b02a09826fc1c1234262b9ccbd
Jun Hyuk Lim <limjunhyuk97@naver.com>
2021-07-02 02:09:01 +0900

    Create 10830.cpp

---
+++ Gold/10830_행렬곱셈과 mod연산/10830.cpp
@@ -0,0 +1,100 @@
+// 수의 거듭제곱에 대한 분할 정복은 지수에 대해서..
+// 행렬의 거듭제곱 꼴에 대한 분할 정복은 어떻게.. : 지수를 분할하여 정복
+
+#include <iostream>
+using namespace std;
+
+typedef long long int ll;
+int N;
+ll B, c;
+ll M[7][7], T0[7][7], T1[7][7];
+
+void multiplyMatrix(ll A[][7], ll B[][7]) {
+
+	if (c++ % 2 == 1) {
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < N; ++j) {
+				T0[i][j] = 0;
+				for (int k = 0; k < N; ++k) {
+					T0[i][j] += (A[i][k] * B[k][j]);
+				}
+				T0[i][j] %= 1000;
+			}
+		}
+		return;
+	}
+	else {
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < N; ++j) {
+				T1[i][j] = 0;
+				for (int k = 0; k < N; ++k) {
+					T1[i][j] += (A[i][k] * B[k][j]);
+				}
+				T1[i][j] %= 1000;
+			}
+		}
+		return;
+	}
+}
+
+void powMatrix(ll n) {
+
+	if (n == 1)
+		return;
+
+	else if (n == 2) {
+		
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < N; ++j) {
+				for (int k = 0; k < N; ++k) {
+					T1[i][j] += (M[i][k] * M[k][j]);
+				}
+				T1[i][j] %= 1000;
+			}
+		}
+		++c;
+		return;
+	}
+
+	if (n % 2 == 0) {
+		powMatrix(n / 2);
+		if (c % 2 == 1)multiplyMatrix(T1, T1);
+		else multiplyMatrix(T0, T0);
+		return;
+	}
+	else {
+		powMatrix(n - 1);
+		if (c % 2 == 1)multiplyMatrix(M, T1);
+		else multiplyMatrix(M, T0);
+		return;
+	}
+
+}
+
+int main(void) {
+
+	cin >> N >> B;
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			cin >> M[i][j];
+		}
+	}
+
+	powMatrix(B);
+
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			if (B == 1)
+				cout << M[i][j] % 1000 << " ";
+			else {
+				if (c % 2 == 0)
+					cout << T0[i][j] << " ";
+				else
+					cout << T1[i][j] << " ";
+			}
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
