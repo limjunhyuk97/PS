b4c57186b3905e969172870dbcbb0d2f0f03fc1c
Jun Hyuk Lim <limjunhyuk97@naver.com>
2021-08-06 19:17:31 +0900

    Create __TEMPLATE__.cpp

---
+++ __TEMPLATE__.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <queue>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+#define INT_INF 2e9
+#define LL_INF 9e18
+#define MOD 1000000007
+using namespace std;
+
+typedef long long int ll;
+
+int main(void){
+    
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    return 0;
+}
+
