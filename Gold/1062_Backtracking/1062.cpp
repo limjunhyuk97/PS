e686f0d7c85923109514f721970733a7a7192817
jun_lim97 <limjunhyuk97@gmail.com>
2021-11-18 23:03:31 +0900

    1062, template change

---
+++ Gold/1062_Backtracking/1062.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <vector>
+#include <utility>
+#include <cmath>
+#include <algorithm>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+#include <bitset>
+#include <cmath>
+#include <array>
+#define INT_INF 2e9
+#define LL_INF 9e18
+#define MOD 1000000007
+using namespace std;
+
+typedef long long int ll;
+
+int main(void){
+    
+    
+    
+    return 0;
+}
+
+/*
+ 50 15
+ antarctica
+ antahellotica
+ antacafsdrtica
+ antarngjocbtica
+ antarnfzojctica
+ antarnaffuctica
+ antarnaapoctica
+ antarnapgoctica
+ antardsfjoctica
+ antanapgoctica
+ antaafsdrctica
+ antarjhgctica
+ antarfjhctica
+ antarfbncttica
+ antarrtctica
+ antarbetrectica
+ antarqreufctica
+ antarrectica
+ antarqfjectica
+ antarafjtica
+ antarqvawectica
+ antarqofwyctica
+ antarvnfjctica
+ antarqovaeutica
+ antareufjectica
+ antarqefjtica
+ antarqefjectica
+ antaructica
+ antarqafdoctica
+ antarqictica
+ antarqerjictica
+ antarqoeictica
+ antarqeeictica
+ antarbroctica
+ antarbuyuoctica
+ antarbvuroctica
+ antarbiuyyctica
+ antarbdsfhctica
+ antarbquroctica
+ antarinfjctica
+ antarirjectica
+ antariqwctica
+ antariwyectica
+ antarigsdvctica
+ antarijectica
+ antarqegafdtica
+ antarqctica
+ antarqgoeictica
+ antarqtqtoetica
+ antarqnuyectica
+ 30
+ */
--- Template/__TEMPLATE__.cpp
+++ Template/__TEMPLATE__.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <cstring>
 #include <queue>
 #include <vector>
 #include <utility>
