594efa695d7bada7a8561dd9c00c9e51eb890725
jun_lim97 <limjunhyuk97@gmail.com>
2022-02-09 22:57:50 +0900

    1388

---
+++ BOJ/DFS-BFS/1388_바닥장식.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+char grid[55][55];
+int N, M, cnt;
+
+int main(void){
+    
+    scanf("%d %d", &N, &M);
+    for(int i=0; i<N; ++i){
+        scanf("%s", grid[i]);
+    }
+    
+    // row check
+    for(int i=0; i<N; ++i){
+        for(int j=0; j<M; ++j){
+            if(grid[][])
+        }
+    }
+    
+    // column check
+    
+    return 0;
+}
