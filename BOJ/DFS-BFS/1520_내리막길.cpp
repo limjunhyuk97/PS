bfc6484efe5c6f9e8ff12e7611443919861e5db9
jun_lim97 <limjunhyuk97@gmail.com>
2022-02-16 10:00:07 +0900

    1520 변경

--- BOJ/DFS-BFS/1520_내리막길.cpp
+++ BOJ/DFS-BFS/1520_내리막길.cpp
@@ -1,5 +1,7 @@
 // 그냥 BFS - 시간초과
 // 들어가는 inflow 0인 놈들만 BFS - 시간초과
+// 10억 개에 육박하는 경로의 수를 세는 것 -> 각 위치 기준으로 한번씩 셀 수 있도록 바꿀 수 있을까?
+
 
 #include <iostream>
 #include <algorithm>
@@ -18,6 +20,7 @@ int dy[4] = {1, -1, 0, 0};
 void calculateInflow(){
     queue<coordinate> tmpQue; tmpQue.push({0, 0});
     set<coordinate> onPath; onPath.insert({0, 0});
+    // BFS
     while(!tmpQue.empty()){
         coordinate cur=tmpQue.front(); tmpQue.pop();
         for(int i=0; i<4; ++i){
@@ -41,37 +44,36 @@ void calculateInflow(){
 }
 
 int BFS(){
+    
+    struct node{
+        int x; int y;
+        bool operator<(const node &other) const{
+            // inflow가 작을수록 먼저 pq 빠져나오도록 고려
+            return inflow[x][y] > inflow[other.x][other.y];
+        }
+    };
+    priority_queue<node> pq;
+    
     int path[505][505];
     for(int i=0; i<505; ++i){
         for(int j=0; j<505; ++j){
             path[i][j]=0;
+            if(inflow[i][j]!=0) pq.push({i, j});
         }
     }
-    path[0][0]=1;
-    set<coordinate> cd; cd.insert({0, 0});
-    while(!cd.empty()){
-        vector<coordinate> targetToErase;
-        for(auto iter=cd.begin(); iter!=cd.end(); ++iter){
-            coordinate cur=*iter;
-            if(inflow[cur.first][cur.second]==0){
-                // 다음것 집어넣는다.
-                for(int i=0; i<4; ++i){
-                    int x=cur.first+dx[i], y=cur.second+dy[i];
-                    if(x<0 || y<0 || x>=M || y>=N) continue;
-                    if(grid[cur.first][cur.second]>grid[x][y]){
-                        inflow[x][y] -= 1;
-                        path[x][y] += path[cur.first][cur.second];
-                        if(path[x][y] != 0) cd.insert({x, y});
-                    }
-                }
-                // 지울 항목에 추가한다.
-                targetToErase.push_back({cur.first, cur.second});
-            }
+    path[0][0]=1; pq.push({0, 0});
+    
+    while(!pq.empty()){
+        node cur = pq.top();
+        for(int i=0; i<4; ++i){
+            int nextX=cur.x+dx[i], nextY=cur.y+dy[i];
+            if(nextX<0 || nextY<0 || nextX>=M || nextY>=N) continue;
+            inflow[nextX][nextY] -= 1;
+            path[nextX][nextY] += path[cur.x][cur.y];
         }
-        for(int i=0; i<targetToErase.size(); ++i){
-            cd.erase(targetToErase[i]);
-        }
+        pq.pop();
     }
+    
     return path[M-1][N-1];
 }
 
