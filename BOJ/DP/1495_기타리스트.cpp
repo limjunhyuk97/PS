8ac4070c1acd62cce585b6d9a940c015e0ea46c4
jun_lim97 <limjunhyuk97@gmail.com>
2022-04-19 10:34:47 +0900

    Directory 구조 변경

--- BOJ/DP/1495_기타리스트.cpp
+++ BOJ/DP/1495_기타리스트.cpp
@@ -1,13 +1,9 @@
-// 2^50의 모든 경우를 고려하는 것이 아니라
-// 가능한 수의 경우로 경우를 강제하는 방법
-
 #include <iostream>
 #include <set>
 using namespace std;
 
 int N, S, M, max_vol = -1;
 int volumes[55];
-bool flag = false;
 set<int> phases[55];
 
 int main(void){
@@ -21,19 +17,10 @@ int main(void){
     
     phases[0].insert(S);
     for(int i=0; i<N; ++i) {
-        if(phases[i].empty()) {
-            flag = true;
-            break;
+        for(auto iter=phases[i].begin(); iter != phases.end(); ++iter) {
+            if(phases[i+1].find(*iter + volumes[i]) == phases[i+1].end() && *iter + volumes[i] < M) phases[i+1].insert(*iter + volumes[i]);
+            if(phases[i+1].find(*iter - volumes[i]) == phases[i+1].end() && *iter - volumes[i] > 0) phases[i+1].insert(*iter - volumes[i]);
         }
-        for(auto iter=phases[i].begin(); iter != phases[i].end(); ++iter) {
-            if(phases[i+1].find(*iter + volumes[i+1]) == phases[i+1].end() && *iter + volumes[i+1] <= M) phases[i+1].insert(*iter + volumes[i+1]);
-            if(phases[i+1].find(*iter - volumes[i+1]) == phases[i+1].end() && *iter - volumes[i+1] >= 0) phases[i+1].insert(*iter - volumes[i+1]);
-        }
-    }
-    
-    if(flag){
-        printf("%d\n", max_vol);
-        return 0;
     }
     
     N -=1 ;
