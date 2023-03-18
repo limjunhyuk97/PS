80baddcf995c67550a7bf250d2a08c8c7be08506
jun_lim97 <limjunhyuk97@gmail.com>
2023-03-18 23:56:34 +0900

    분할정복 silver 한문제 해결

--- source.cpp
+++ source.cpp
@@ -1,79 +1,43 @@
 #include <iostream>
-#include <cmath>
-#define INF 20000000
+#include <algorithm>
 using namespace std;
 
-struct co{
-    int x;
-    int y;
-    int operator- (const co &other) const {
-        return abs(x - other.x) + abs(y - other.y);
-    }
-};
+int N;
+int grid[1025][1025];
 
-int N, M, cuts, min_dist = INF;
-int house_cnt = 0, chicken_cnt = 0;
-int dist[105][15];
-
-co houses[105];
-co chickens[15];
-
-bool checked[15];
-
-void get_min_dist(int n, int m) {
-    int total_dist = 0;
-    for(int i=0; i<n; ++i) {
-        int row_min = INF;
-        for(int j=0; j<m; ++j) {
-            if(checked[j]) continue;
-            row_min = row_min > dist[i][j] ? dist[i][j] : row_min;
-        }
-        total_dist += row_min;
-    }
-    min_dist = min_dist > total_dist ? total_dist : min_dist;
-}
-
-void dfs(int cnt, int last) {
-    if(cnt == 0) {
-        get_min_dist(house_cnt, chicken_cnt);
-    }
+// 왼쪽 위 (x1, y1) ~ 오른쪽 아래 (x2, y2)
+int pooling(int x1, int y1, int x2, int y2) {
+    if(x1==x2 && y1 == y2) return grid[x1][y1];
     
-    for(int i=last; i<chicken_cnt; ++i) {
-        checked[i] = true;
-        dfs(cnt - 1, i+1);
-        checked[i] = false;
-    }
+    int len = x2 - x1 + 1;
+    int h = len / 2;
+    int d = h - 1;
+    
+    int s1 = pooling(x1, y1, x1 + d, y1 + d);
+    int s2 = pooling(x1, y1 + h, x1 +d, y1 + h + d);
+    int s3 = pooling(x1 + h, y1, x1 + h + d, y1 + d);
+    int s4 = pooling(x1 + h, y1 + h, x1 + h + d, y1 + h + d);
+    
+    int cand[4] = {s1, s2, s3, s4};
+    
+    sort(&cand[0], &cand[4]);
+    
+    return cand[2];
 }
 
 int main(void) {
     
-    // dist 초기화
-    for(int i=0; i<105; ++i) {
-        for(int j=0; j<15; ++j) {
-            dist[i][j] = INF;
-        }
-    }
+    scanf("%d", &N);
     
-    scanf("%d %d", &N, &M);
-    
+    // 데이터 입력 받기
     for(int i=0; i<N; ++i) {
         for(int j=0; j<N; ++j) {
-            int tmp; scanf("%d", &tmp);
-            if(tmp == 1) houses[house_cnt++] = {i, j};
-            if(tmp == 2) chickens[chicken_cnt++] = {i, j};
+           scanf("%d", &grid[i][j]);
         }
     }
     
-    for(int i=0; i<house_cnt; ++i) {
-        for(int j=0; j<chicken_cnt; ++j) {
-            dist[i][j] = houses[i] - chickens[j];
-        }
-    }
-    
-    cuts = chicken_cnt - M;
-    dfs(cuts, 0);
-    
-    printf("%d\n", min_dist);
+    printf("%d\n", pooling(0, 0, N-1, N-1));
     
     return 0;
+    
 }
