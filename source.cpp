d6513d49bf83fed3568958c4e22167f661ea1428
jun_lim97 <limjunhyuk97@gmail.com>
2023-04-08 12:48:12 +0900

    시뮬레이션 구현 시도중 (구슬탈출)

--- source.cpp
+++ source.cpp
@@ -1,135 +1,346 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-#define MAXN 15
-#define MAXK 15
+#include <string>
+#define MAXLEN 15
 using namespace std;
 
-struct horse {
-    int x;
-    int y;
-    int d;
+//cout << r.first << "," << r.second << "/" <<b.first << "," << b.second <<endl;
+//5 7
+//#######
+//#R..#B#
+//#.O...#
+//#.....#
+//#######
+
+typedef pair<int, int> co;
+
+struct node {
+    co r;
+    co b;
 };
 
-int dx[5] = {0, 0, 0, -1, 1};
-int dy[5] = {0, 1, -1, 0, 0};
-int inverted[5] = {0, 2, 1, 4, 3};
+int N, M;
+char grid[MAXLEN][MAXLEN];
 
-int color_grid[MAXN][MAXN];
-deque<int> horse_grid[MAXN][MAXN];
-horse horses[MAXK];
+co rpos, bpos;
 
-int N, K;
-int tick = -1;
-
-void initColorGrid() {
-    for(int i=0; i<MAXN; ++i) {
-        for(int j=0; j<MAXN; ++j) {
-            color_grid[i][j] = -1;
-        }
-    }
-}
-
-bool OOB(int x, int y) {
-    if(x < 1 || y < 1 || x > N || y > N) return true;
-    else return false;
-}
-
-bool run() {
+// true : R만 , false : B만 또는, R-B둘다, 아무도 안빠짐
+bool left(node cur, queue<node>& que) {
+    co r = cur.r, b = cur.b;
     
-    for(int idx=1; idx<=K; ++idx) {
-        int d = horses[idx].d;
-        int x = horses[idx].x;
-        int y = horses[idx].y;
-        int nx = x + dx[d];
-        int ny = y + dy[d];
-        
-        if(OOB(nx, ny) || color_grid[nx][ny] == 2) {
-            d = inverted[d];
-            nx = x + dx[d];
-            ny = y + dy[d];
+    bool movement = false;
+    bool was_hole = false;
+    // r 먼저 움직이는 경우
+    if(r.second < b.second) {
+        bool flag = false;
+        // r 움직이기
+        while(grid[r.first][r.second-1] != '#') {
+            r.second -= 1;
+            movement = true;
+            // R 낙
+            if(grid[r.first][r.second] == 'O') {
+                flag = true;
+                was_hole = true;
+            }
+        }
+        // r 공빠지면 drop 처리
+        if(!was_hole) {
+            grid[r.first][r.second] = 'R';
+        }
+    
+        // b 움직이기
+        while(grid[b.first][b.second-1] != '#' && grid[b.first][b.second-1] != 'R') {
+            b.second -= 1;
+            movement = true;
+            // R 낙 B 낙 , B 낙
+            if(grid[b.first][b.second] == 'O') return false;
         }
         
-        // white
-        if(color_grid[nx][ny] == 0) {
-            // 내 위에 있는 것들만 추출
-            deque<int> move_target;
-            while(horse_grid[x][y].back() != idx) {
-                int other = horse_grid[x][y].back();
-                move_target.push_back(other);
-                horse_grid[x][y].pop_back();
-                // 따라온놈 위치 변경
-                horses[other]={nx, ny, horses[other].d};
-            }
-            move_target.push_back(idx);
-            horse_grid[x][y].pop_back();
-            // 이동
-            while(!move_target.empty()) {
-                horse_grid[nx][ny].push_back(move_target.back());
-                move_target.pop_back();
-            }
-            // 4 이상이면 탈출
-            if(horse_grid[nx][ny].size() > 3) return true;
-            // 내 위치 변경
-            horses[idx] = {nx, ny, d};
-            
-        }
-        
-        // red
-        else if(color_grid[nx][ny] == 1) {
-            // 내 위에 있는 것들만 뒤집어서 데려간다.
-            while(horse_grid[x][y].back() != idx) {
-                int other = horse_grid[x][y].back();
-                horse_grid[nx][ny].push_back(other);
-                horse_grid[x][y].pop_back();
-                // 따라온 놈 위치 변경
-                horses[other]={nx, ny, horses[other].d};
-            }
-            horse_grid[nx][ny].push_back(idx);
-            horse_grid[x][y].pop_back();
-            // 4 이상이면 탈출
-            if(horse_grid[nx][ny].size() > 3) return true;
-            // 내 위치 변경
-            horses[idx] = {nx, ny, d};
-        }
-        
-        // blue of OOB
+        if(!was_hole) grid[r.first][r.second] = '.';
+        // R 낙
+        if(flag) return true;
+        // 아무도 안 낙
         else {
-            // 아무데도 안움직임 . 방향만 전환
-            horses[idx] = {x, y, d};
+            if(movement) que.push({r, b});
+            return false;
+        }
+    }
+    // b 먼저 움직이는 경우
+    else {
+        // b 움직이기
+        while(grid[b.first][b.second-1] != '#') {
+            b.second -= 1;
+            movement = true;
+            // R 낙 B 낙 , B 낙
+            if(grid[b.first][b.second] == 'O') return false;
+        }
+        grid[b.first][b.second] = 'B';
+        // r 움직이기
+        while(grid[r.first][r.second-1] != '#' && grid[r.first][r.second-1] != 'B') {
+            r.second -= 1;
+            movement = true;
+            // R 낙
+            if(grid[r.first][r.second] == 'O') return true;
+        }
+        grid[b.first][b.second] = '.';
+        // 아무도 안 낙
+        if(movement) que.push({r, b});
+        return false;
+    }
+}
+
+bool right(node cur, queue<node>& que) {
+    co r = cur.r, b = cur.b;
+    bool movement = false;
+    bool was_hole = false;
+    // r 먼저 움직이는 경우
+    if(r.second > b.second) {
+        bool flag = false;
+        // r 움직이기
+        while(grid[r.first][r.second+1] != '#') {
+            r.second += 1;
+            movement = true;
+            // R 낙
+            if(grid[r.first][r.second] == 'O') {
+                flag = true;
+                was_hole = true;
+            }
+        }
+        // r 공빠지면 drop 처리
+        if(!was_hole) {
+            grid[r.first][r.second] = 'R';
+        }
+        // b 움직이기
+        while(grid[b.first][b.second+1] != '#' && grid[b.first][b.second+1] != 'R') {
+            b.second += 1;
+            movement = true;
+            // R 낙 B 낙 , B 낙
+            if(grid[b.first][b.second] == 'O') return false;
+        }
+        if(!was_hole)grid[r.first][r.second] = '.';
+        // R 낙
+        if(flag) return true;
+        // 아무도 안 낙
+        else {
+            if(movement) que.push({r, b});
+            return false;
+        }
+    }
+    // b 먼저 움직이는 경우
+    else {
+        // b 움직이기
+        while(grid[b.first][b.second+1] != '#') {
+            b.second += 1;
+            movement = true;
+            // R 낙 B 낙 , B 낙
+            if(grid[b.first][b.second] == 'O') return false;
+        }
+        grid[b.first][b.second] = 'B';
+        
+        // r 움직이기
+        while(grid[r.first][r.second+1] != '#' && grid[r.first][r.second+1] != 'B') {
+            r.second += 1;
+            movement = true;
+            // R 낙
+            if(grid[r.first][r.second] == 'O') return true;
+        }
+        grid[b.first][b.second] = '.';
+        // 아무도 안 낙
+        if(movement) que.push({r, b});
+        return false;
+    }
+}
+
+bool up(node cur, queue<node> &que) {
+    co r = cur.r, b = cur.b;
+    bool movement = false;
+    bool was_hole = false;
+    // r 먼저 움직이는 경우
+    if(r.first < b.first) {
+        bool flag = false;
+        // r 움직이기
+        while(grid[r.first-1][r.second] != '#') {
+            r.first -= 1;
+            movement = true;
+            // R 낙
+            if(grid[r.first][r.second] == 'O') {
+                was_hole = true;
+                flag = true;
+            }
+        }
+        // r 공빠지면 drop 처리
+        if(!was_hole) {
+            grid[r.first][r.second] = 'R';
+        }
+        // b 움직이기
+        while(grid[b.first-1][b.second] != '#' && grid[b.first-1][b.second] != 'R') {
+            b.first -= 1;
+            movement = true;
+            // R 낙 B 낙 , B 낙
+            if(grid[b.first][b.second] == 'O') return false;
+        }
+        if(!was_hole)grid[r.first][r.second] = '.';
+        // R 낙
+        if(flag) return true;
+        // 아무도 안 낙
+        else {
+            if(movement)que.push({r, b});
+            return false;
+        }
+    }
+    // b 먼저 움직이는 경우
+    else {
+        // b 움직이기
+        while(grid[b.first-1][b.second] != '#') {
+            b.first -= 1;
+            movement = true;
+            // R 낙 B 낙 , B 낙
+            if(grid[b.first][b.second] == 'O') return false;
+        }
+        grid[b.first][b.second] = 'B';
+        // r 움직이기
+        while(grid[r.first-1][r.second] != '#' && grid[r.first-1][r.second] != 'B') {
+            r.first -= 1;
+            movement = true;
+            // R 낙
+            if(grid[r.first][r.second] == 'O') return true;
+        }
+        grid[b.first][b.second] = '.';
+        // 아무도 안 낙
+        if(movement)que.push({r, b});
+        return false;
+    }
+}
+
+bool down(node cur, queue<node> & que) {
+    co r = cur.r, b = cur.b;
+    bool movement = false;
+    bool was_hole = false;
+    // r 먼저 움직이는 경우
+    if(r.first > b.first) {
+        bool flag = false;
+        // r 움직이기
+        while(grid[r.first+1][r.second] != '#') {
+            r.first += 1;
+            movement = true;
+            // R 낙
+            if(grid[r.first][r.second] == 'O') {
+                flag = true;
+                was_hole = true;
+            }
+        }
+        // r 공빠지면 drop 처리
+        if(!was_hole) {
+            grid[r.first][r.second] = 'R';
+        }
+        // b 움직이기
+        while(grid[b.first+1][b.second] != '#' && grid[b.first+1][b.second] != 'R') {
+            b.first += 1;
+            movement = true;
+            // R 낙 B 낙 , B 낙
+            if(grid[b.first][b.second] == 'O') return false;
+        }
+        if(!was_hole)grid[r.first][r.second] = '.';
+        // R 낙
+        if(flag) return true;
+        // 아무도 안 낙
+        else {
+            if(movement)que.push({r, b});
+            return false;
+        }
+    }
+    // b 먼저 움직이는 경우
+    else {
+        // b 움직이기
+        while(grid[b.first+1][b.second] != '#') {
+            b.first += 1;
+            movement = true;
+            // R 낙 B 낙 , B 낙
+            if(grid[b.first][b.second] == 'O') return false;
+        }
+        grid[b.first][b.second] = 'B';
+        // r 움직이기
+        while(grid[r.first+1][r.second] != '#' && grid[r.first+1][r.second] != 'B') {
+            r.first += 1;
+            movement = true;
+            // R 낙
+            if(grid[r.first][r.second] == 'O') return true;
+        }
+        grid[b.first][b.second] = '.';
+        // 아무도 안 낙
+        if(movement) que.push({r, b});
+        return false;
+    }
+}
+
+//cout << r.first << "," << r.second << "/" <<b.first << "," << b.second <<endl;
+//for(int i=0; i<N; ++i) {
+//    for(int j=0; j<M; ++j) {
+//        printf("%c ", grid[i][j]);
+//    }
+//    printf("\n");
+//}
+
+int run() {
+    int tick = 0;
+    queue<node> que;
+    que.push({rpos, bpos});
+    
+    bool flag = false;
+    while(!que.empty() && !flag) {
+        int size = (int)que.size();
+        
+        tick += 1;
+        if(tick > 10) break;
+        
+        cout << tick << endl;
+//        cout << (flag ? "true" : "false")<< endl;
+//        cout << size << endl;
+        for(int i=0; i<size; ++i) {
+            node cur = que.front(); que.pop();
+             cout << cur.r.first << "," << cur.r.second << "/" <<cur.b.first << "," << cur.b.second <<endl <<endl;
+            if(left(cur, que)) {
+                flag = true;
+                break;
+            }
+            if(right(cur, que)) {
+                flag = true;
+                break;
+            }
+            if(up(cur, que))  {
+                flag = true;
+                break;
+            }
+            if(down(cur, que)) {
+                flag = true;
+                break;
+            }
         }
     }
     
-    
-    return false;
+    return flag ? tick : -1;
 }
 
 int main(void) {
     
-    scanf("%d %d", &N, &K);
-    
-    initColorGrid();
-    
-    for(int i=1; i<=N; ++i) {
-        for(int j=1; j<=N; ++j) {
-            scanf("%d", &color_grid[i][j]);
+    scanf("%d %d", &N, &M);
+    for(int i=0; i<N; ++i) {
+        string str; cin >> str;
+        for(int j=0; j<M; ++j) {
+            grid[i][j] = str[j];
+            if(grid[i][j] == 'R') {
+                grid[i][j] = '.';
+                rpos = {i, j};
+            }
+            if(grid[i][j] == 'B') {
+                grid[i][j] = '.';
+                bpos = {i, j};
+            }
         }
     }
     
-    for(int i=1; i<=K; ++i) {
-        int x, y, d; scanf("%d %d %d", &x, &y, &d);
-        horses[i] = {x, y, d};
-        horse_grid[x][y].push_back(i);
-    }
-    
-    for(int time=1; time <=1000; ++time) {
-        if(run()) {
-            tick = time;
-            break;
-        }
-    }
-    
-    printf("%d\n", tick);
+    int turn = run();
+    printf("%d\n", turn);
     
     return 0;
 }
